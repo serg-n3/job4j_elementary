16fefdea21992ef706f31ad0c142247950ce1a9e
serg-n3 <ktg80@yandex.ru>
2022-09-11 06:30:29 +0300

    Простое число [#171694 #341185]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,18 +2,14 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number == 1) {
-            prime = false;
-
-        } else {
+        boolean prime = number > 1;
             for (int i = 2; i < number - 1; i++) {
                 if (number % i == 0) {
                     prime = false;
                     break;
                 }
             }
-        }
+
             return prime;
 
     }
