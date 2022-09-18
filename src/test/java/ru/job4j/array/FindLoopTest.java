4387ca375c4baf1e9c8bdf65a9c9cfb053bcb766
ktg95 <ktg80@yandex.ru>
2022-09-18 13:42:12 +0300

    Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
-        int[] data = new int[] {5, 10, 3};
+        int[] data = new int[]{5, 10, 3};
         int el = 5;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
@@ -14,10 +14,23 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenArrayHasNot10ThenMinus1() {
-        int[] data = new int[] {5, 10, 3};
-        int el = 6;
-        int result = FindLoop.indexOf(data, el);
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        int[] data = new int[]{5, 6, 7, 10, 9};
+        int el = 8;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
         int expected = -1;
         assertThat(result).isEqualTo(expected);
     }
