4390df52aa8d14e398926d3629fd7b67a6ae0c1d
ktg95 <ktg80@yandex.ru>
2022-09-27 19:05:16 +0300

    Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,17 +2,17 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean firstSym = !name.isEmpty() && isLowerLatinLetter(name.codePointAt(0));
-        if (firstSym) {
+        boolean result = !name.isEmpty() && isLowerLatinLetter(name.codePointAt(0));
+        if (result) {
             for (int i = 1; i < name.length(); i++) {
                 int code = name.codePointAt(i);
                 if (!(Character.isDigit(code) || isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code))) {
-                    firstSym = false;
+                    result = false;
                     break;
                 }
             }
         }
-        return firstSym;
+        return result;
     }
 
     public static boolean isSpecialSymbol(int code) {
